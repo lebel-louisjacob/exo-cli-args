407e4909a36b91e0ba4ec31825b77a59c1c3710a
Louis-Jacob <lebel.louisjacob@gmail.com>
2021-11-15 13:12:47 +0000

    api 0.2.2

--- src/arguments.cpp
+++ src/arguments.cpp
@@ -1,15 +1,15 @@
 #include <native.hpp>
 
-exo::Object character_to_binary(char const character, exo::Context const& context) {
-    auto const&& bit_library { exo::find_parent("bit", context) };
-    auto const&& true_bit { exo::get_property("true", bit_library, context) };
-    auto const&& false_bit { exo::get_property("false", bit_library, context) };
+exo::Object character_to_binary(char const character, exo::Object const& self) {
+    auto const&& bit_library { exo::find("bit", self) };
+    auto const&& true_bit { exo::get("true", bit_library) };
+    auto const&& false_bit { exo::get("false", bit_library) };
 
-    auto const&& stack_library { exo::find_parent("stack", context) };
-    auto const&& empty_stack { exo::get_property("empty", stack_library, context) };
+    auto const&& stack_library { exo::find("stack", self) };
+    auto const&& empty_stack { exo::get("empty", stack_library) };
 
-    auto const&& binary_library { exo::find_parent("binary", context) };
-    auto const&& binary_constructor { exo::get_property("of", binary_library, context) };
+    auto const&& binary_library { exo::find("binary", self) };
+    auto const&& binary_constructor { exo::get("of", binary_library) };
 
     auto bit_stack { empty_stack };
 
@@ -17,36 +17,36 @@ exo::Object character_to_binary(char const character, exo::Context const& contex
         auto const&& index_is_one { character & (0b1 << (character_index - 1)) };
         auto const& bit_value { index_is_one ? true_bit : false_bit };
 
-        auto const&& push_function { exo::get_property("push", bit_stack, context) };
-        bit_stack = exo::call(bit_value, push_function, context);
+        auto const&& push_function { exo::get("push", bit_stack) };
+        bit_stack = exo::call(bit_value, push_function);
     }
 
-    return exo::call(bit_stack, binary_constructor, context);
+    return exo::call(bit_stack, binary_constructor);
 }
 
-exo::Object c_string_to_string(char const* character_address, exo::Context const& context) {
-    auto const&& stack_library { exo::find_parent("stack", context) };
-    auto const&& empty_stack { exo::get_property("empty", stack_library, context) };
+exo::Object c_string_to_string(char const* character_address, exo::Object const& self) {
+    auto const&& stack_library { exo::find("stack", self) };
+    auto const&& empty_stack { exo::get("empty", stack_library) };
 
-    auto const&& string_library { exo::find_parent("string", context) };
-    auto const&& string_constructor { exo::get_property("of", string_library, context) };
+    auto const&& string_library { exo::find("string", self) };
+    auto const&& string_constructor { exo::get("of", string_library) };
 
     auto character_stack { empty_stack };
 
     while (char const character = *character_address) {
-        auto const&& binary_character { character_to_binary(character, context) };
-        auto const&& push_function { exo::get_property("push", character_stack, context) };
-        character_stack = exo::call(binary_character, push_function, context);
+        auto const&& binary_character { character_to_binary(character, self) };
+        auto const&& push_function { exo::get("push", character_stack) };
+        character_stack = exo::call(binary_character, push_function);
 
         character_address += sizeof(character);
     }
 
-    return exo::call(character_stack, string_constructor, context);
+    return exo::call(character_stack, string_constructor);
 }
 
-exo::Object value_property(exo::Context const context) {
-    auto const&& stack_library { exo::find_parent("stack", context) };
-    auto const&& empty_stack { exo::get_property("empty", stack_library, context) };
+exo::Object value_property(exo::Object const self) {
+    auto const&& stack_library { exo::find("stack", self) };
+    auto const&& empty_stack { exo::get("empty", stack_library) };
 
     auto const&& argc { exo::get_argc() };
     auto const&& argv { exo::get_argv() };
@@ -54,14 +54,14 @@ exo::Object value_property(exo::Context const context) {
     auto argument_stack { empty_stack };
 
     for (uint32_t argv_index { argc }; argv_index > 0; --argv_index) {
-        auto const&& character_string { c_string_to_string(argv[argv_index - 1], context) };
-        auto const&& push_function { exo::get_property("push", argument_stack, context) };
-        argument_stack = exo::call(character_string, push_function, context);
+        auto const&& character_string { c_string_to_string(argv[argv_index - 1], self) };
+        auto const&& push_function { exo::get("push", argument_stack) };
+        argument_stack = exo::call(character_string, push_function);
     }
 
     return argument_stack;
 }
 
-extern "C" void create_native_object(exo::ConstructionContext const construction_context, exo::Context const context) {
-    exo::define_property("value", &value_property, construction_context);
+extern "C" void create_native_object(exo::Context const context, exo::Object const self) {
+    exo::define_property("value", &value_property, context);
 }
